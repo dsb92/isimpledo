0cfb4a242af737b96f487984e1ed0b81abe60d6e
David Buhauer <davidbuhauer@Davids-MacBook-Pro.local>
2015-01-16 18:44:32 +0100

    Initial Commit

---
+++ iOSDoToList/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iOSDoToList
+//
+//  Created by David Buhauer on 16/01/15.
+//  Copyright (c) 2015 David Buhauer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
